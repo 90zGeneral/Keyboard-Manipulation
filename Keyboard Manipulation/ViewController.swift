37f7cfef33fb26cbad7307868eb721ab3f91227d
Roydon Jeffrey <brushdam14823@gmail.com>
2016-06-11 13:37:34 -0400

    Removing the keyboard upon 'return' key press and tapping anywhere outside of the textField area

--- Keyboard Manipulation/ViewController.swift
+++ Keyboard Manipulation/ViewController.swift
@@ -8,16 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//UITextFieldDelegate will allow the viewController to control it
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var textField: UITextField!
+    
+    @IBOutlet var label: UILabel!
+    
+    @IBAction func action(sender: AnyObject) {
+        
+        //to update label with textField value
+        label.text = textField.text
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //the viewController is responsible for this particular textField
+        self.textField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        //this access the view in the view controller and removes the keyboard upon click
+        self.view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        //this removes the keyboard upon 'return' key press
+        textField.resignFirstResponder()
+        
+        return true
+        
     }
 
 
